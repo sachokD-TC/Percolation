62a04775af62624d4dad5288797e801e4cddb19e
SachokD-TC <Rythm1977>
2018-04-12 17:33:38 +0300

    working program

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,84 +1,146 @@
-import edu.princeton.cs.algs4.QuickUnionUF;
 import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Percolation {
 
     private int[][] percolationMatrix;
-    private int numberOfSites = 1;
-    private int maxnum;
-    private QuickUnionUF quickUnionUF;
-    private int[][] openSites;
+    private int[][] rootNumbers;
+    private int numberOfSites = 0;
+    private int n;
+    private WeightedQuickUnionUF quickUnionUF;
+
 
     public Percolation(int n) {
-        maxnum = n+2;
-        quickUnionUF = new QuickUnionUF(n*n+2);
-        for(int i=0; i!=n; i++){
-            quickUnionUF.connected(0,i);
-            quickUnionUF.connected(maxnum, n-i);
-        }
-        openSites = new int[n][n];
+        this.n = n;
+        quickUnionUF = new WeightedQuickUnionUF(n*n+2);
+        rootNumbers = new int[n][n];
         percolationMatrix = new int[n][n];
+        int s=1;
         for (int i = 0; i != n; i++) {
             for (int m = 0; m != n; m++) {
                 percolationMatrix[i][m] = 1;
+                rootNumbers[i][m] = s;
+                s++;
             }
         }
+        getOpenSitesWhenPercolate(n);
     }
 
     public void open(int row, int col) {
+        validateRowCol(row, col);
+        row--;
+        col--;
         percolationMatrix[row][col] = 0;
-        openSites[row][col] = numberOfSites;
-        if(numberOfSites > 1) {
-            checkForConnectionAndAdd(row, col);
-        }
+        checkForConnectionAndAdd(row, col);
         numberOfSites++;
     }    // open site (row, col) if it is not open already
 
     private void checkForConnectionAndAdd(int row, int col){
-        if(row-1 > 0 && openSites[row-1][col] != 0){
-            quickUnionUF.union(openSites[row-1][col],openSites[row][col]);
+        if(row == 0){
+            quickUnionUF.union(0, rootNumbers[row][col]);
         }
-        if(row+1 < openSites.length && openSites[row+1][col] != 0){
-            quickUnionUF.union(openSites[row+1][col],openSites[row][col]);
+        if(row == this.n-1) {
+            quickUnionUF.union(n*n+1, rootNumbers[row][col]);
         }
-        if(col-1 > 0 && openSites[row][col-1] != 0){
-            quickUnionUF.union(openSites[row][col-1],openSites[row][col]);
+        if(row-1 >= 0 && percolationMatrix[row-1][col] == 0){
+            quickUnionUF.union(rootNumbers[row-1][col], rootNumbers[row][col]);
         }
-        if(col+1 < openSites.length && openSites[row][col+1] != 0){
-            quickUnionUF.union(openSites[row][col+1],openSites[row][col]);
+        if(row+1 < rootNumbers.length && percolationMatrix[row+1][col] == 0){
+            quickUnionUF.union(rootNumbers[row+1][col], rootNumbers[row][col]);
+        }
+        if(col-1 >= 0 && percolationMatrix[row][col-1] == 0){
+            quickUnionUF.union(rootNumbers[row][col-1], rootNumbers[row][col]);
+        }
+        if(col+1 < rootNumbers.length && percolationMatrix[row][col+1] == 0){
+            quickUnionUF.union(rootNumbers[row][col+1], rootNumbers[row][col]);
         }
     }
 
     public boolean isOpen(int row, int col) {
+        validateRowCol(row, col);
+        row--;
+        col--;
         return percolationMatrix[row][col] == 0;
     }  // is site (row, col) open?
 
     public boolean isFull(int row, int col) {
+        validateRowCol(row, col);
+        row--;
+        col--;
         return percolationMatrix[row][col] == 1;
     }  // is site (row, col) full?
 
     public int numberOfOpenSites() {
-        return numberOfSites-1;
+        return numberOfSites;
     } // number of open sites
 
     public boolean percolates() {
-        return quickUnionUF.connected(0, maxnum);
+        return quickUnionUF.connected(0, this.n*this.n+1);
     } // does the system percolate?
 
 
     public static void main(String[] args) {
         int n = Integer.valueOf(args[0]);
         Percolation percolation = new Percolation(n);
-        for(int i=0; i!=50; i++){
-            int row = StdRandom.uniform(n);
-            int col = StdRandom.uniform(n);
-            if(percolation.isFull(row, col)){
-                percolation.open(row, col);
-            }
-        }
+        System.out.println(percolation.getOpenSitesWhenPercolate(n));
         System.out.println(percolation);
-        System.out.println(percolation.percolates());
     }// test client (optional)
+
+    private int getOpenSitesWhenPercolate(int n){
+        while(!percolates()){
+            while(!setOpenSite(n));
+        }
+        return numberOfOpenSites();
+    }
+
+    private boolean setOpenSite(int n){
+        int row = StdRandom.uniform(1,n+1);
+        int col = StdRandom.uniform(1,n+1);
+        if(this.isFull(row, col)){
+            this.open(row, col);
+            return true;
+        }
+        return false;
+    }
+
+    private void validateRowCol(int row, int col) {
+        if (row <= 0 || col <= 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+
+    private void readMatrix(){
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("matrix.txt"));
+            String line = reader.readLine();
+            int m=0;
+            int n=0;
+            while(line!=null){
+                for(int i=0; i!=line.length(); i++){
+                    if(!(line.charAt(i) == ' ')) {
+                        percolationMatrix[n][m] = Integer.valueOf("" +line.charAt(i));
+                        if(percolationMatrix[n][m] == 0){
+                            open(n,m);
+                        }
+                        m++;
+                    }
+                }
+                n++;
+                m=0;
+                line = reader.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public String toString() {
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,38 +1,44 @@
-import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    int[] openSites;
+    private double[] openSitesPercent;
+    private int trials;
+
     public PercolationStats(int n, int trials) {
-        int m=0;
-        openSites = new int[trials];
-        for(int i=0; i!= trials; i++) {
+        this.trials = trials;
+        openSitesPercent = new double[trials];
+        for (int i = 0; i != trials; i++) {
             Percolation percolation = new Percolation(n);
-            for (int k = 0; k != n; i++) {
-                int row = StdRandom.uniform(n);
-                int col = StdRandom.uniform(n);
-                if (percolation.isFull(row, col)) {
-                    percolation.open(row, col);
-                }
-            }
-            if(percolation.percolates()){
-                openSites[m] = percolation.numberOfOpenSites();
-                m++;
-            }
+            openSitesPercent[i] = percolation.numberOfOpenSites() / Math.pow(n, 2);
         }
     }   // perform trials independent experiments on an n-by-n grid
 
 
-
-    public double mean()     {
-        return StdStats.mean(openSites);
+    public double mean() {
+        return StdStats.mean(openSitesPercent);
     }                     // sample mean of percolation threshold
-    public double stddev()    {return 0;}                    // sample standard deviation of percolation threshold
-    public double confidenceLo(){return 0;}                  // low  endpoint of 95% confidence interval
-    public double confidenceHi() {return 0;}                 // high endpoint of 95% confidence interval
 
-    public static void main(String[] args)  {
-        PercolationStats ps = new PercolationStats(200, 100);
+    public double stddev() {
+        return StdStats.stddev(openSitesPercent);
+    }                    // sample standard deviation of percolation threshold
+
+    public double confidenceLo() {
+        return mean() - 1.96 * stddev() / this.trials;
+    }                  // low  endpoint of 95% confidence interval
+
+    public double confidenceHi() {
+        return mean() - 1.96 * stddev() / this.trials;
+    }                 // high endpoint of 95% confidence interval
+
+    public static void main(String[] args) {
+        int n = Integer.valueOf(args[0]);
+        int trial = Integer.valueOf(args[1]);
+        if (n <= 0 || trial <= 0) {
+            throw new IllegalArgumentException();
+        }
+        PercolationStats ps = new PercolationStats(n, trial);
         System.out.println("mean = " + ps.mean());
+        System.out.println("stddev = " + ps.stddev());
+        System.out.println("95% confidence interval = [" + ps.confidenceLo() + ", " + ps.confidenceHi() + "]");
     }      // test client (described below)
 }
